284057db9a8e756b1152be2f5053031149f3c7c6
PFilippov1 <p.filippov2.20@gmail.com>
2025-03-26 17:52:08 +0200

    feat: cart layout and items in cart disabling when deleting

--- next-pizza/app/api/cart/route.ts
+++ next-pizza/app/api/cart/route.ts
@@ -90,10 +90,9 @@ export async function POST(req: NextRequest) {
 
     const updatedUserCart = await updateCartTotalAmount(token);
     const resp = NextResponse.json(updatedUserCart);
-    
+
     resp.cookies.set('cartToken', token);
     return resp;
-
   } catch (error) {
     console.log('[CART_POST] Server error', error);
     return NextResponse.json({ message: 'Failed to create a cart' }, { status: 500 });
